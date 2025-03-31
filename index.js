5e375c5645a26ff7d7795886c5ad47acb7030d1b
Мухамедов Надир Рамилевич <n.mukhamedov@mkit.local>
2025-03-31 12:52:06 +0300

    Add get Endpoint

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express()
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.send(`hello world`)})
+    res.send(`hello world`)
+})
 
+app.get('/name',(req,res)=>{
+    const name = 'Надир';
+    const familyname = 'Мухамедов' 
+    return res.send(`${name} ${familyname}`);
+})
 app.listen(process.env.PORT || 8080, () => console.log(`Server start no ${process.env.PORT || 8080}`))
