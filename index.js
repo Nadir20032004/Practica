1b71d12986a1aaf0f7c17d8d45160c6c25d0de6a
Мухамедов Надир Рамилевич <n.mukhamedov@mkit.local>
2025-03-31 12:55:17 +0300

    POST endpoint

--- index.js
+++ index.js
@@ -11,5 +11,20 @@ app.get('/name',(req,res)=>{
     const name = 'Надир';
     const familyname = 'Мухамедов' 
     return res.send(`${name} ${familyname}`);
-})
-app.listen(process.env.PORT || 8080, () => console.log(`Server start no ${process.env.PORT || 8080}`))
+});
+app.post('/',async(req,res)=>{
+    const {login,password} = req.body
+    await User.create({login,password})
+    return res.send(`User was created`)
+});
+async function start() {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(process.env.PORT || 8080, () => console.log(`Server start no ${process.env.PORT || 8080}`))
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
