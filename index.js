bc209b28d91b50c9eea709f7120ba677d99272ba
Мухамедов Надир Рамилевич <n.mukhamedov@mkit.local>
2025-03-31 12:49:32 +0300

    Web-Server

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send(`hello world`)})
+
+app.listen(process.env.PORT || 8080, () => console.log(`Server start no ${process.env.PORT || 8080}`))
